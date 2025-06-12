46adaf20e87453955f73584aa65137e04fd4df08
maryam ahmadi <mrymrghb82@gmail.com>
2025-06-12 14:55:59 +0330

    Delete src/middlewares/validatePolygon.js

--- src/middlewares/validatePolygon.js
+++
@@ -1,7 +0,0 @@
-/*export const isRingClosed = (rings) => {
-  if (!Array.isArray(rings) || rings.length < 4) return false;
-  const [lon1, lat1] = rings[0];
-  const [lon2, lat2] = rings[rings.length - 1];
-  return lon1 === lon2 && lat1 === lat2;
-};
-*/
